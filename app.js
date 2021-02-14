c3f637a7ae7ec3255d1ab267e1691f2705b3a379
deedatbilla <deedat5@yahoo.com>
2021-02-14 18:42:36 +0000

    added socket feature that allows user to send data from phone to pc

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-require('dotenv').config()
-const express = require('express') 
-require('./Database/db')
-const userRouter = require('./Routes/auth')
-const deviceRouter = require('./Routes/device')
-const clipBoard = require('./Routes/clipBoard')
-const port = process.env.PORT
-const app = express()
-app.use(express.json()) 
-app.use(userRouter)
-app.use(deviceRouter)
-app.use(clipBoard)
-app.use('/public', express.static('public'));
-app.use((req, res, next) => { 
-    // Error goes via `next()` method
-    setImmediate(() => {
-        next(new Error('Something went wrong'));
-    });
+require("dotenv").config();
+const express = require("express");
+require("./Database/db");
+const userRouter = require("./Routes/auth");
+const deviceRouter = require("./Routes/device");
+const clipBoard = require("./Routes/clipBoard");
+const port = process.env.PORT;
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+app.use(express.json());
+app.use(userRouter);
+app.use(deviceRouter);
+app.use(clipBoard);
+app.use("/public", express.static("public"));
+app.use((req, res, next) => {
+  // Error goes via `next()` method
+  setImmediate(() => {
+    next(new Error("Something went wrong"));
+  });
 });
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 app.use(function (err, req, res, next) {
-    console.error(err.message);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
 });
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-   
-}) 
+  console.log(`Server running on port ${port}`);
+});
+const devices = {};
+io.on("connection", (socket) => {
+  
+   //when user sends clipboard from phone to pcs
+  //phone will emit the user's id, clipboard data and pc list
+  //pcs will be listening here
+  socket.on("from_phone", (data) => {
+    const { userid, pcs } = data;
+    // socket.on(`user-pc${userid}`,)
+    pcs.forEach((element) => {
+      socket.broadcast.emit(`to_pc-${element.id}-${userid}`, data);
+    });
+  });
+
+ 
+});
