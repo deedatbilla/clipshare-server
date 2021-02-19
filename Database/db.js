38b89e611de3be5fa7947a715748606bdc525b64
deedatbilla <deedat5@yahoo.com>
2021-02-19 20:56:38 +0000

    fixed issue with database

--- Database/db.js
+++ Database/db.js
@@ -1,21 +1,21 @@
-// var MongoClient = require('mongodb').MongoClient;
-const mongoose = require("mongoose");
-const url = process.env.DB_URL;
-mongoose.connect(url, { useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology:true });
-const db = mongoose.connection;
-db.once("open", (_) => {
-  console.log("Database connected:", url);
-});
+var MongoClient = require('mongodb').MongoClient;
+// const mongoose = require("mongoose");
+const url = process.env.LIVE_DB_URL;
+// mongoose.connect(url, { useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology:true });
+// const db = mongoose.connection;
+// db.once("open", (_) => {
+//   console.log("Database connected:", url);
+// });
 
-db.on("error", (err) => {
-  console.error("connection error:", err);
-});
-//  const dbName = 'clipshate'
-//  MongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true }, (err, client) => {
-//     if (err) return console.log(err)
+// db.on("error", (err) => {
+//   console.error("connection error:", err);
+// });
+ const dbName = 'Clypsync'
+ MongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true }, (err, client) => {
+    if (err) return console.log(err)
 
-//     // Storing a reference to the database so you can use it later
-//     db = client.db(dbName)
-//     console.log(`Connected MongoDB: ${url}`)
-//     console.log(`Database: ${dbName}`)
-//   })
+    // Storing a reference to the database so you can use it later
+    db = client.db(dbName)
+    console.log(`Connected MongoDB: ${url}`)
+    console.log(`Database: ${dbName}`)
+  })
