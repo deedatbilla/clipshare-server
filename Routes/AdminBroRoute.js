93fdfe4c60cde4522b19e885e3192b0d380514f1
deedatbilla <deedat5@yahoo.com>
2021-03-13 15:43:52 +0000

    misc

--- Routes/AdminBroRoute.js
+++ Routes/AdminBroRoute.js
@@ -14,7 +14,7 @@ const app = express();
 const adminBro = new AdminBro({
   // databases: [],
   //   rootPath: "/admin",
-  resources: [clipBoard, User, Subscription,Admin],
+  resources: [ User, Subscription,Admin],
 });
 
 // const router = AdminBroExpress.buildRouter(adminBro);
